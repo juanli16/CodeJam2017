352823bb04d818ac7f4a794d2bbbfbf9bca3ef7c
Justin <juan.y.li@mail.mcgill.ca>
2017-11-18 04:18:42 +0000

    Time conversion algo is good

--- timeconv.py
+++ timeconv.py
@@ -1,14 +1,14 @@
-#!usr/bin/python3
+#!/usr/bin/python3
 
 import pandas as pd
 import numpy as np
 import sys
 
 try:
-	file = sys.argv[1]
-	print("Processing ", file)
+    file = sys.argv[1]
+    print("Processing ", file)
 except IndexError:
-	print("Need file name as argument")
+    print("Need file name as argument")
 
 #month dictionary:
 m = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
@@ -16,18 +16,28 @@ m = {1:31, 2:28, 3:31, 4:30, 5:31, 6:30, 7:31, 8:31, 9:30, 10:31, 11:30, 12:31}
 
 data = pd.read_csv(file)
 
-time = data['time']
+time = data['starttime']
 time = time.values
 
-time_conv = []
-date_conv = []
-for i in range(len(time)):
-	tmp = time[i]
-	sec = int(tmp[17:19])
-	mins = int(tmp[14:16])
-	h = int(tmp[11:13])
-	date  = int(tmp[8:10])
-	month = m[int(tmp[5:7])]
-	year = int(tmp[0:4])-2014
-	time_conv.append( sec + mins*60 + h*3600 )
-	date_conv.append(date)
+def convertTime(time):
+    time_conv = []
+    date_conv = []
+    for i in range(len(time)):
+        tmp = time[i]
+        sec = int(tmp[17:19])
+        mins = int(tmp[14:16])
+        h = int(tmp[11:13])
+        date  = int(tmp[8:10])      
+        month = m[int(tmp[5:7])]
+        year = (int(tmp[2:4])-14)
+        if year%4==0:
+            year = year * 366
+        else:
+            year = year * 365
+        
+        time_conv.append( sec + mins*60 + h*3600 )
+        date_conv.append(date + month + year)
+
+    return time_conv, date_conv
+
+print(convertTime(time[:2]))
